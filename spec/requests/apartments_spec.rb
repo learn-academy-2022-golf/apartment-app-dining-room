13b8c3c39bb6308c0bc38afc92d3ca965ff335da
Mannyplg <pettigrew.i@gmail.com>
2023-01-06 15:32:19 -0800

    validations complete

--- spec/requests/apartments_spec.rb
+++ spec/requests/apartments_spec.rb
@@ -1,4 +1,28 @@
 require "rails_helper"
-
 RSpec.describe "Apartments", type: :request do
-end
+    describe "GET /index" do
+      it "gets a list of apartments " do
+      
+        user = User.where(email: 'test@test.test').first_or_create(password: '12345678', password_confirmation: '12345678')
+        user.apartments.create(
+          street: "Pine Island Ave.",
+          city:"Spectacular",
+          state:"CA",
+          manager:"Garrett Graham",
+          email:"graham.g@aol.com", 
+          price:"1M", 
+          bedrooms:2, 
+          bathrooms:1, 
+          pets:"yes",
+          image:"url", 
+          user_id:1
+        )
+        # Make a request
+        get '/apartments'
+        apartments = JSON.parse(response.body)
+        expect(response).to have_http_status(200)
+        expect(apartment.length).to eq 1
+      end
+    end
+  end
+
