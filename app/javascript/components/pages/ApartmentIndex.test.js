d89e8f716c6d2b879c03871bce51c2cde3e37fce
Andre Jordan Gahan <107956416+ajgahan19@users.noreply.github.com>
2023-01-06 16:16:52 -0800

    finished unprotexted index

--- app/javascript/components/pages/ApartmentIndex.test.js
+++ app/javascript/components/pages/ApartmentIndex.test.js
@@ -1,10 +1,17 @@
 import React from "react"
 import { render } from "@testing-library/react"
 import ApartmentIndex from "./ApartmentIndex"
+import { BrowserRouter } from "react-router-dom"
+import { screen } from "@testing-library/dom"
+import mockApartments from "../mockApartments"
 
 describe("<ApartmentIndex />", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div")
-    render(<ApartmentIndex />, div)
+    render(<BrowserRouter><ApartmentIndex apartments={mockApartments} /></BrowserRouter>, div)
+    mockApartments.forEach(element => {
+      const apartment = screen.getByText("Location; " + element.street + " " + element.city + ", " + element.state)
+      expect(apartment).toBeInTheDocument()
+    });
   })
 })
