fff5152c791e78bdeb5b5712508ad1bf0c28c763
tabithatrubey <tabithatrubey@outlook.com>
2023-01-10 14:25:08 -0800

    finished protected created frontend

--- app/javascript/components/pages/ApartmentNew.js
+++ app/javascript/components/pages/ApartmentNew.js
@@ -1,9 +1,151 @@
-import React from "react"
+import React, { useState } from "react"
+import {Form, FormGroup, Label, Input, Button} from "reactstrap"
+import { useNavigate } from "react-router-dom"
 
-const ApartmentNew = () => {
+
+const ApartmentNew = ({ createApartment }) => {
+  const navigate = useNavigate()
+  const [newApartment, setNewApartment] = useState({
+    street: "",
+    city: "",
+    state: "",
+    manager: "",
+    email: "",
+    price: "",
+    bedrooms: "",
+    bathrooms: "",
+    pets: "",
+    image: "",
+    user_id: 4
+  })
+  
+  const handleChange = (e) => {
+    setNewApartment({...newApartment, [e.target.name]: e.target.value})
+  }
+
+  const handleSubmit = () => {
+    createApartment(newApartment)
+    navigate("/apartmentindex")
+  }
+
   return (
     <>
-      <h3>ApartmentNew</h3>
+      <h1>Create a new apartment</h1>
+      <Form>
+        <FormGroup>
+          <Label for="street">
+            Street
+          </Label>
+          <Input
+            name="street"
+            onChange={handleChange}
+            placeholder="What is your street?" 
+            type="text"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="city">
+            City
+          </Label>
+          <Input
+            name="city"
+            onChange={handleChange}
+            placeholder="What is your city?"
+            type="text"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="state">
+            State
+          </Label>
+          <Input
+            name="state"
+            onChange={handleChange}
+            placeholder="What is your state?"
+            type="text"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="manager">
+            Manager
+          </Label>
+          <Input
+            name="manager"
+            onChange={handleChange}
+            placeholder="Who is your manager?"
+            type="text"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="email">
+            Email
+          </Label>
+          <Input
+            name="email"
+            onChange={handleChange}
+            placeholder="What is your email?"
+            type="text"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="price">
+            Price
+          </Label>
+          <Input
+            name="price"
+            onChange={handleChange}
+            placeholder="What is your ideal price?"
+            type="text"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="bedrooms">
+            Bedrooms
+          </Label>
+          <Input
+            name="bedrooms"
+            onChange={handleChange}
+            placeholder="How many bedrooms?"
+            type="number"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="bathrooms">
+            Bathrooms
+          </Label>
+          <Input
+            name="bathrooms"
+            onChange={handleChange}
+            placeholder="How many bathrooms?"
+            type="number"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="pets">
+            Pets
+          </Label>
+          <Input
+            name="pets"
+            onChange={handleChange}
+            placeholder="How many pets?"
+            type="number"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="image">
+            Image
+          </Label>
+          <Input
+            name="image"
+            onChange={handleChange}
+            placeholder="Please share your image."
+            type="url"
+          />
+        </FormGroup>
+        <Button onClick={handleSubmit}>
+          Submit Apartment
+        </Button>
+      </Form>
     </>
   )
 }
